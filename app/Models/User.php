c0182d6754e046e1063a7af88e88e278a9f3c5c6
Fedor <doworkupwell@gmail.com>
2022-06-23 22:33:31 +0300

    update ide-helper

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,14 +11,14 @@ use Laravel\Passport\HasApiTokens;
 /**
  * App\Models\User
  *
- * @property      int $id
- * @property      string $first_name
- * @property      string $last_name
- * @property      string $email
- * @property      string $password
- * @property      \Illuminate\Support\Carbon|null $created_at
- * @property      \Illuminate\Support\Carbon|null $updated_at
- * @property      int $role_id
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $password
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $role_id
  * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Passport\Client[] $clients
  * @property-read int|null $clients_count
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
@@ -26,21 +26,21 @@ use Laravel\Passport\HasApiTokens;
  * @property-read \App\Models\Role $role
  * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Passport\Token[] $tokens
  * @property-read int|null $tokens_count
- * @method        static \Database\Factories\UserFactory factory(...$parameters)
- * @method        static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
- * @method        static \Illuminate\Database\Eloquent\Builder|User newQuery()
- * @method        static \Illuminate\Database\Eloquent\Builder|User query()
- * @method        static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
- * @method        static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
- * @method        static \Illuminate\Database\Eloquent\Builder|User whereFirstName($value)
- * @method        static \Illuminate\Database\Eloquent\Builder|User whereId($value)
- * @method        static \Illuminate\Database\Eloquent\Builder|User whereLastName($value)
- * @method        static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
- * @method        static \Illuminate\Database\Eloquent\Builder|User whereRoleId($value)
- * @method        static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
- * @mixin         \Eloquent
- * @property      int $is_influencer
- * @method        static \Illuminate\Database\Eloquent\Builder|User whereIsInfluencer($value)
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRoleId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ * @property int $is_influencer
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsInfluencer($value)
  */
 class User extends Authenticatable
 {
